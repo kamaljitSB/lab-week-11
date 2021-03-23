7f9f861cdf2a1ba44bc5a40bd3c12e3cc3e170a7
Kamaljit Bhullar Desktop <kamaljitbhuller@gmail.com>
2021-03-22 17:13:07 -0700

    finished the lab for week 11, all works as expected.

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,31 @@
-// your JavaScript goes here, you can delete this comment
+
+const todoList = [
+    {
+        todo: "Clone the repo",
+        status: "started"
+    },
+    {
+        todo: "make a commit",
+        status: "completed"
+    },
+    {
+        todo: "Submit the repo link",
+        status: "completed"
+    }
+]
+const completedTodos = []
+
+for (const element in todoList){
+    if (todoList[element].status === "completed"){
+        completedTodos.push(todoList[element])
+    }
+}
+console.log(completedTodos)
+
+const [{todo: a, status:ab}, {todo:b, status:ba}] = completedTodos
+
+for (const item in [{todo: a, status:ab}, {todo:b, status:ba}]){
+    console.log([{todo: a, status:ab}, {todo:b, status:ba}][item].todo)
+}
+
+
